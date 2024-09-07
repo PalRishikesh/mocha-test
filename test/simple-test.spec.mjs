7f83e34df60c23c5d9bbcb4ce47e0fbdfea11d2b
Abhishek Kumar Gupta <63952465+abhikumar002@users.noreply.github.com>
2024-09-07 18:10:47 +0530

    test

--- test/simple-test.spec.mjs
+++ test/simple-test.spec.mjs
@@ -50,3 +50,46 @@ describe.skip('Simple test Suite',()=>{
 
     
 })
+
+JS
+Node
+Mysql
+Mongodb
+
+
+console.log("One");
+setImmediate(()=>{
+    console.log("Five");
+})
+setTimeout(() => {
+    console.log("Two");
+}, 0);
+
+setTimeout(() => {
+    console.log("Three");
+},2000);
+
+console.log("Four");
+
+
+indexing
+projection
+aggrate function
+how to connect different tables
+Lookup
+
+
+
+// * One, Four, Two, Five,  Three */
+// console.log("2" + 2);
+// console.log("2" - 2);
+
+let arr=[1,2,3,3,5,4,2,5]
+
+{
+    1:1,
+    2:2,
+    3:2,
+    4:1,
+    5:1
+}
